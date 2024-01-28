6dd27ca74b2bf76f48e94b97122e39a899fbb4ad
Musab1Blaser <musab16000@gmail.com>
2024-01-28 21:38:25 +0500

    Create Blank screen

--- game.py
+++ game.py
@@ -4,13 +4,18 @@ import random
 def rand_strat(game_state, options):
     return random.sample(options, 1)
 
-def run_game(strat1, strat2):
+def run_game(strat1, strat2): # provide the two strategy functions
+    game_state = [[0]*7 for i in range(6)] # generate empty board
+    
     pygame.init()
-    game_state = [[0]*7 for i in range(6)]
-    screen = pygame.display.set_mode((800, 800))
-    # while True:
-
-
+    screen = pygame.display.set_mode((800, 800)) # create screen
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+        pygame.display.update()
 
 
 run_game(rand_strat, rand_strat)
